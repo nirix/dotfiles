e20fc6a8f0c7f26c45481e4dec58c9714ddb9d82
Jack Polgar <jack@polgar.id.au>
2013-10-27 16:25:53 +1100

    Improved fish theme git status.

--- fish/themes/wat/fish_prompt.fish
+++ fish/themes/wat/fish_prompt.fish
@@ -12,10 +12,11 @@ function fish_prompt
   set -l red (set_color -o red)
   set -l blue (set_color -o blue)
   set -l green (set_color -o green)
+  set -l magenta (set_color -o magenta)
   set -l normal (set_color normal)
 
   set -l arrow "$normal> "
-  #set -l cwd $cyan(basename (prompt_pwd))
+  # set -l cwd $cyan(basename (prompt_pwd))
   set -l cwd $cyan(echo $PWD | sed -e 's,'"$HOME"',~,g')
 
   set -l user $red(whoami) ' '
@@ -24,10 +25,36 @@ function fish_prompt
     set -l git_branch (_git_branch_name)
     set git_info "$green ["$blue"git"$green":$git_branch]"
 
-    if [ (_is_git_dirty) ]
-      set -l dirty "$red ✗"
-      set git_info "$git_info$dirty"
+    # Dirty
+    # if [ (_is_git_dirty) ]
+    #   set -l dirty "$red ✗"
+    #   set git_info "$git_info$dirty"
+    # end
+
+    # Added
+    if [ (git status -s | grep -E '^[A]| [A] ' ^/dev/null ) ]
+      set git_info "$git_info $green✚"
     end
+
+    # Deleted
+    if [ (git status -s | grep -E '^[D]| [D] ' ^/dev/null ) ]
+      set git_info "$git_info $red✖"
+    end
+
+    # Modified
+    if [ (git status -s | grep -e '^[M]| [M] ' ^/dev/null ) ]
+      set git_info "$git_info $blue✱"
+    end
+
+    # Renamed / renamed and deleted
+    if [ (git status -s | grep -E '^[R]|[R][D] ' ^/dev/null ) ]
+      set git_info "$git_info $magenta➜"
+    end
+
+    # Unstaged
+    # if [ (git status -s | grep -E '^\?\? ' ^/dev/null ) ]
+    #   set git_info "$git_info $normal◼"
+    # end
   end
 
   echo -n -s $user "$normal"$cwd $git_info $normal '
