f0d411c48d5312dced51e165b8e73c9ea3a937e2
Jack Polgar <jack@polgar.id.au>
2019-11-30 00:37:29 +1100

    Update dotfiles

--- install.sh
+++ install.sh
@@ -48,10 +48,10 @@ elif [ $SHELL = "bash" ]; then
   bash shells/bash/install.bash
 fi
 
-read -p "What OS? [linux/osx]" $OS
+read -p "What OS? [linux/osx]" OS
 
 if [ $OS = "linux" ]; then
-  bash config/link_linux.sh
+  # bash config/link_linux.sh
 fi
 
 install
--- shells/zsh/install.zsh
+++ shells/zsh/install.zsh
@@ -1,7 +1,7 @@
 #read -p "oh-my-zsh, zim or prezto? [omz/zim/prezto] " zshframework
 
 #if [ $SHELL = "omz" ]; then
-  zsh oh-my-zsh/install.zsh
+  zsh "${HOME}/.dotfiles/shells/zsh/oh-my-zsh/install.zsh"
 #elif [ $SHELL = "zim" ]; then
 #  zsh zim/install.zsh
 #elif [ $SHELL = "prezto" ]; then
